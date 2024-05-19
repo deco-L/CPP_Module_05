7104a4cba916ae2eae1bc3237186d01d79868f58
deco-L <scotchwitchbroom@gmail.com>
2024-05-19 19:58:17 +0900

    add color code in Makefile ꒰𑁬(⸝⸝＞ᯅ＜⸝⸝)໒꒱

--- ex00/src/Bureaucrat.cpp
+++ ex00/src/Bureaucrat.cpp
@@ -6,24 +6,24 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/19 18:51:58 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/05/19 19:47:39 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void) {
+// Bureaucrat::Bureaucrat(void) {
 
-}
+// }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& obj) {
+// Bureaucrat::Bureaucrat(const Bureaucrat& obj) {
 
-}
+// }
 
-Bureaucrat::~Bureaucrat(void) {
+// Bureaucrat::~Bureaucrat(void) {
 
-}
+// }
 
-Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
+// Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
 
-}
+// }
