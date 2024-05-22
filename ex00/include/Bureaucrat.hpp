15b14ac801a2b60b87955027e5f8eea57e7b4a6b
deco-L <scotchwitchbroom@gmail.com>
2024-05-22 18:43:26 +0900

    V V

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/19 18:50:48 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/05/22 16:34:25 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 #include <cstdlib>
 #include <iostream>
 
-class Bureaucrat
-{
-public:
-  Bureaucrat( void );
-  Bureaucrat( const Bureaucrat& obj );
-  ~Bureaucrat( void );
+// class Bureaucrat
+// {
+// public:
+//   Bureaucrat( void );
+//   Bureaucrat( const Bureaucrat& obj );
+//   ~Bureaucrat( void );
 
-  Bureaucrat& operator=( const Bureaucrat& obj );
-};
+//   Bureaucrat& operator=( const Bureaucrat& obj );
+// };
 
 #endif
