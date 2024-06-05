efe59c3b146c6f96f1657005c8be89d045cc142e
deco-L <scotchwitchbroom@gmail.com>
2024-06-05 20:50:12 +0900

    ꒰𑁬(⸝⸝×ᯅ×⸝⸝)໒꒱

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/22 16:34:25 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/06/05 16:00:41 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,33 @@
 #include <cstdlib>
 #include <iostream>
 
-// class Bureaucrat
-// {
-// public:
-//   Bureaucrat( void );
-//   Bureaucrat( const Bureaucrat& obj );
-//   ~Bureaucrat( void );
+class Bureaucrat
+{
+private:
+  const std::string _name;
+  int               _grade;
 
-//   Bureaucrat& operator=( const Bureaucrat& obj );
-// };
+public:
+  Bureaucrat( void );
+  Bureaucrat( const std::string& name );
+  Bureaucrat( const std::string& name, int grade );
+  Bureaucrat( const Bureaucrat& obj );
+  ~Bureaucrat();
+
+  class GradeTooHighException : public std::exception {
+  public:
+    virtual const char* what( void ) const throw();
+  };
+
+  class GradeTooLowException : public std::exception {
+  public:
+    virtual const char* what( void ) const throw();
+  };
+
+  const std::string&  getName( void ) const;
+  int                 getGrade( void ) const;
+
+  Bureaucrat& operator=( const Bureaucrat& obj );
+};
 
 #endif
--- ex00/src/Bureaucrat.cpp
+++ ex00/src/Bureaucrat.cpp
@@ -6,24 +6,62 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/19 19:47:39 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/06/05 16:19:11 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "../include/Bureaucrat.hpp"
 
-// Bureaucrat::Bureaucrat(void) {
+Bureaucrat::Bureaucrat(void):_name(""), _grade()
+{
 
-// }
+}
 
-// Bureaucrat::Bureaucrat(const Bureaucrat& obj) {
+Bureaucrat::Bureaucrat(const std::string& name):_name(name), _grade()
+{
 
-// }
+}
 
-// Bureaucrat::~Bureaucrat(void) {
+Bureaucrat::Bureaucrat(const std::string& name, int grade):_name(name), _grade(grade)
+{
 
-// }
+}
 
-// Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
+Bureaucrat::Bureaucrat(const Bureaucrat& obj):_name(obj.getName())
+{
+  *this = obj;
+}
 
-// }
+Bureaucrat::~Bureaucrat(void)
+{
+  std::cout << "\e[1;92mBureaucrat deconstructor called.\n";
+}
+
+const char* Bureaucrat::GradeTooHighException::what(void) const throw() {
+  std::cout << "Grade too high.\n";
+}
+
+const char* Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+  std::cout << "Grade too low.\n";
+}
+
+const std::string&  Bureaucrat::getName(void) const
+{
+  return (this->_name);
+}
+
+int  Bureaucrat::getGrade(void) const
+{
+  return (this->_grade);
+} 
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj)
+{
+  if (this != &obj) {
+    this->_grade = obj.getGrade();
+  }
+    std::cout << "\e[1;31mError: "
+              << "Attempted self-assignment in copy assignment operator.\e[0m"
+              << std::endl;
+}
